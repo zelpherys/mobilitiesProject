b441a97f5e1aaf43196b7856d6c24a4f39aeb0ff
zelpherys <bregeresullivan@outlook.fr>
2023-11-15 13:29:26 +0100

    first commit

--- src/Controller/DashboardAdminController.php
+++ src/Controller/DashboardAdminController.php
@@ -31,39 +31,39 @@ class DashboardAdminController extends AbstractController
         ]);
     }
 
-    #[Route('/dashboard/admin/supprimer-post/{id}', name: 'supprimer_post_admin', methods: ['GET', 'POST'])]
-    public function supprimerPost(Request $request, $id, EntityManagerInterface $entityManager, Security $security): Response
-    {
-        // Si l'utilisateur n'est pas connecté en tant qu'admin, redirigez-le vers la page de connexion admin
-        if (!$security->isGranted('ROLE_ADMIN')) {
-            return $this->redirectToRoute('app_admin_connection');
-        }
+    // #[Route('/dashboard/admin/supprimer-post/{id}', name: 'supprimer_post_admin', methods: ['GET', 'POST'])]
+    // public function supprimerPost(Request $request, $id, EntityManagerInterface $entityManager, Security $security): Response
+    // {
+    //     // Si l'utilisateur n'est pas connecté en tant qu'admin, redirigez-le vers la page de connexion admin
+    //     if (!$security->isGranted('ROLE_ADMIN')) {
+    //         return $this->redirectToRoute('app_admin_connection');
+    //     }
 
-        $post = $entityManager->getRepository(Post::class)->find($id);
+    //     $post = $entityManager->getRepository(Post::class)->find($id);
 
-        if (!$post) {
-            throw $this->createNotFoundException('Le post n\'existe pas.');
-        }
+    //     if (!$post) {
+    //         throw $this->createNotFoundException('Le post n\'existe pas.');
+    //     }
 
-        // Supprimez d'abord les enregistrements dans la table avispost liés à ce post
-        $avisposts = $entityManager->getRepository(Avispost::class)->findBy(['idPost' => $post]);
+    //     // Supprimez d'abord les enregistrements dans la table avispost liés à ce post
+    //     $avisposts = $entityManager->getRepository(Avispost::class)->findBy(['idPost' => $post]);
 
-        foreach ($avisposts as $avispost) {
-            $entityManager->remove($avispost);
-        }
+    //     foreach ($avisposts as $avispost) {
+    //         $entityManager->remove($avispost);
+    //     }
 
-        // Supprimez les commentaires liés à ce post
-        $commentaires = $entityManager->getRepository(Commentaire::class)->findBy(['idPost' => $post]);
+    //     // Supprimez les commentaires liés à ce post
+    //     $commentaires = $entityManager->getRepository(Commentaire::class)->findBy(['idPost' => $post]);
 
-        foreach ($commentaires as $commentaire) {
-            $entityManager->remove($commentaire);
-        }
+    //     foreach ($commentaires as $commentaire) {
+    //         $entityManager->remove($commentaire);
+    //     }
 
-        // Supprimez le post
-        $entityManager->remove($post);
-        $entityManager->flush();
+    //     // Supprimez le post
+    //     $entityManager->remove($post);
+    //     $entityManager->flush();
 
-        // Redirigez l'utilisateur vers la page d'accueil du tableau de bord admin
-        return $this->redirectToRoute('app_dashboard_admin');
-    }
+    //     // Redirigez l'utilisateur vers la page d'accueil du tableau de bord admin
+    //     return $this->redirectToRoute('app_dashboard_admin');
+    // }
 }
--- src/Controller/VoirPostAdminController.php
+++ src/Controller/VoirPostAdminController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,11 +14,40 @@ class VoirPostAdminController extends AbstractController
     #[Route('/voir-post-admin/{id}', name: 'app_voir_post_admin')]
     public function index(PostRepository $postRepo,$id): Response
     {
-        //créer une nouvelle variable -> on appelle le repo ->
         $posts = $postRepo -> findByIdUtlisateur($id);
-    
+        
         return $this->render('voir_post_admin/index.html.twig', [
             'posts' => $posts,
         ]);
     }
+
+    #[Route('/supprimer-post-admin/{id}', name: 'app_supprimer_post_admin')]
+    
+    public function supprimerPost(
+        PostRepository $postRepo,
+        EntityManagerInterface $em,
+        Request $request,
+        $id
+    ): Response {
+        $postId = (int) $id;
+    
+        // Supprimer tous les avis liés à ce post
+        $sqlDeleteAvis = "DELETE FROM avispost WHERE id_post = :postId";
+        $em->getConnection()->executeStatement($sqlDeleteAvis, ['postId' => $postId]);
+    
+        // Supprimer tous les commentaires liés à ce post
+        $sqlDeleteComments = "DELETE FROM commentaire WHERE id_post = :postId";
+        $em->getConnection()->executeStatement($sqlDeleteComments, ['postId' => $postId]);
+    
+        // Supprimer le post
+        $sqlDeletePost = "DELETE FROM post WHERE id_post = :postId";
+        $em->getConnection()->executeStatement($sqlDeletePost, ['postId' => $postId]);
+    
+        $this->addFlash('success', 'Le post et tous ses commentaires ont été supprimés avec succès.');
+    
+        // Redirection vers la page précédente
+        return $this->redirect($request->headers->get('referer'));
+    }
 }
+    
+
